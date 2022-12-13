a7973fd7cc0dec03cb7db2ee1246e915683c17b4
Anna Tsolakou <anna.tsolakou@amadeus.com>
2022-12-13 18:02:06 +0100

    update covid app

--- covid19/covid_app.py
+++ covid19/covid_app.py
@@ -6,32 +6,6 @@ from openapi_client.model.meta import Meta
 from openapi_client.model.warning import Warning
 from pprint import pprint
 
-import time
-import openapi_client
-from pprint import pprint
-from openapi_client.apis.tags import amadeus_o_auth2_access_token_api
-from openapi_client.model.amadeus_amadeus_amadeus_o_auth2_token import AmadeusAmadeusAmadeusOAuth2Token
-from openapi_client.model.amadeus_amadeus_error_response import AmadeusAmadeusErrorResponse
-# Defining the host is optional and defaults to https://test.api.amadeus.com/v1
-# See configuration.py for a list of all supported configuration parameters.
-configuration = openapi_client.Configuration(
-    host = "https://test.api.amadeus.com/v1"
-)
-
-
-# Enter a context with an instance of the API client
-with openapi_client.ApiClient(configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = amadeus_o_auth2_access_token_api.AmadeusOAuth2AccessTokenApi(api_client)
-    access_token = "access_token_example" # str | 
-
-    try:
-        # The OAuth 2.0 token info endpoint
-        api_response = api_instance.get_oauth2_token_info(access_token)
-        pprint(api_response)
-    except openapi_client.ApiException as e:
-        print("Exception when calling AmadeusOAuth2AccessTokenApi->get_oauth2_token_info: %s\n" % e)
-
 configuration = openapi_client.Configuration(
     host = "https://test.api.amadeus.com/v2"
 )
@@ -42,10 +16,15 @@ with openapi_client.ApiClient(configuration) as api_client:
     query_params = {
         'countryCode': "US",
     }
+    
+    header_params = {
+    'Authorization': "Bearer MY_ACCESS_TOKEN",
+    }
     try:
         api_response = api_instance.g_et_covid_report(
             query_params=query_params,
+            header_params=header_params
         )
-        print(api_response)
+        pprint(api_response)
     except openapi_client.ApiException as e:
-        print("Exception: %s\n" % e)
+        print("Exception when calling Covid19AreaReportApi->g_et_covid_report: %s\n" % e)
